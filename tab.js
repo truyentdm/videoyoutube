e2895517a2793768a86f11e684dc7d28c03e4d4f
truyentdm <truyentdm@gmail.com>
2020-09-05 17:52:22 +0700

    Update tab.js

--- tab.js
+++ tab.js
@@ -1,4 +1,4 @@
-function openCity(evt, cityName) {
+function openCity(evt, cityName,css ="w3-red") {
   var i, x, tablinks;
   x = document.getElementsByClassName("city");
   for (i = 0; i < x.length; i++) {
@@ -6,8 +6,8 @@ function openCity(evt, cityName) {
   }
   tablinks = document.getElementsByClassName("tablink");
   for (i = 0; i < x.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace(" w3-red", "");
+    tablinks[i].className = tablinks[i].className.replace(css, "");
   }
   document.getElementById(cityName).style.display = "block";
-  evt.currentTarget.className += " w3-red";
+  evt.currentTarget.className += css;
 }
