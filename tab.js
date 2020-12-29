a8fbe3b02dcfdcffeafbc94e4801f9f48464f465
truyentdm <truyentdm@gmail.com>
2020-12-29 23:23:19 +0700

    Update tab.js

--- tab.js
+++ tab.js
@@ -1,20 +1,6 @@
-function ppYoutube(pause=true){
-  if(pause){
-     $('.youtube-video').each((i,item)=>{
-      	$('.youtube-video')[i].contentWindow.postMessage('{"event":"command","func":"' + 'pauseVideo' + '","args":""}', '*');
-      })
-  }else{
-    //play
-    $('.youtube-video').each((i,item)=>{
-      	$('.youtube-video')[i].contentWindow.postMessage('{"event":"command","func":"' + 'playVideo' + '","args":""}', '*');
-    });
-  }
-}
-function openCity(evt, cityName,css =" w3-primary",video=false) {
+
+function openCity(evt, cityName,css =" w3-primary") {
   var i, x, tablinks;
-  if(video){
-    //ppYoutube(true);
-  }
   x = document.getElementsByClassName("city");
   for (i = 0; i < x.length; i++) {
     x[i].style.display = "none";
